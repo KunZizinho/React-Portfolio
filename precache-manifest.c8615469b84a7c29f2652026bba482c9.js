b62e2266e5e8a2609db5cadcb9d943aa67c84060
KunZizinho <kun.zizinho@gmail.com>
2020-08-21 11:00:26 -0400

    Updates

---
+++ precache-manifest.c8615469b84a7c29f2652026bba482c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8649616c69b5e5f236fa8bd9ba0d0213",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "449fd45e3219388bc369",
+    "url": "/React-Portfolio/static/css/main.9fe26fd9.chunk.css"
+  },
+  {
+    "revision": "128d5e6c15f193a840fc",
+    "url": "/React-Portfolio/static/js/2.a01512a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.a01512a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449fd45e3219388bc369",
+    "url": "/React-Portfolio/static/js/main.1b87c982.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "08519d21f9cc9af9923105c8d92fc81e",
+    "url": "/React-Portfolio/static/media/CD14419E-77DC-42A9-BE51-6DE573B3EE31.08519d21.jpg"
+  }
+]);
